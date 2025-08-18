6732f46d0f1f695b2b5bc4c2c35c3e68f1de9f64
Avokadiy <braaedy1@mail.ru>
2025-08-18 21:26:23 +0500

    deploy fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 const Footer = () => {
     return (
       <footer className="bg-[#49321D] text-white py-4">
         <div className="container mx-auto text-center">
           <p>&copy; 2025 Ваша Компания. Все права защищены.</p>
           <div className="mt-2">
-            <a href="/terms" className="text-white mx-2">Условия</a>
-            <a href="/privacy" className="text-white mx-2">Политика конфиденциальности</a>
+            <Link href="/terms">
+              <p className="text-white mx-2">Условия</p>
+            </Link>
+            <Link href="/privacy">
+              <p className="text-white mx-2">Условия</p>
+            </Link>
           </div>
         </div>
       </footer>
