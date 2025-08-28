d2946fe206821d802f6c0d61d9e569cafca4e822
Avokadiy <braaedy1@mail.ru>
2025-08-28 20:25:57 +0500

    build fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
       <footer className="bg-[#49321D] text-white py-4">
+        
         <div className="container mx-auto text-center">
           <p>&copy; 2025 Ваша Компания. Все права защищены.</p>
           <div className="mt-2">
