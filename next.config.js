420244e30559996cbf3c8dc12fca0d11e32274ec
Vadim <70364152+xPROGAx@users.noreply.github.com>
2025-08-30 20:22:18 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '.next',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'jlihetezqlvilhysgsxi.supabase.co',
-          pathname: '/storage/v1/object/public/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'drive.google.com',
-          pathname: '**'
-        }
-      ],
-    },
-  };
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'jlihetezqlvilhysgsxi.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '**'
+      }
+    ],
+  }
+};
 
-  export default nextConfig;
+export default nextConfig;
