cbeaee034d46e92de4c1acd0bd8a203f497d7151
Vadim <70364152+xPROGAx@users.noreply.github.com>
2025-09-18 14:41:02 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Header from "../components/Header";
-import ProductsSection from "../components/ProductsSection"; // Импортируем компонент
+import ProductsSection from "../components/ProductsSection";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <section className="text-center py-2">
-        <h2 className="text-4xl font-bold text-[#9E0E05]">Лучшие букеты и клубника в шоколаде</h2>
+      <section className="text-center py-4 md:py-6 lg:py-8">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-[#9E0E05] px-4">
+          Лучшие букеты и клубника в шоколаде
+        </h2>
       </section>
-      <ProductsSection id="products" category="popular" title="Популярные товары" /> {/* Добавляем компонент ProductsSection */}
+      <ProductsSection id="products" category="popular" title="Популярные товары" />
     </main>
   );
 }
